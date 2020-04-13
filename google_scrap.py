e2364d44195c3c8f884773e55244c69e7b4d99c5
javibg96 <blascogarcia.javier@outlook.com>
2020-04-13 18:46:03 +0200

    web scraping learning

--- google_scrap.py
+++ google_scrap.py
@@ -2,7 +2,8 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 
-query = "hora en nueva york"
+ciudad = "Dublin"
+query = "hora en " + ciudad
 query = query.replace(' ', '+')
 URL = f"https://google.com/search?q={query}"
 
@@ -14,3 +15,26 @@ MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) App
 
 headers = {"user-agent": USER_AGENT}
 resp = requests.get(URL, headers=headers)
+if resp.status_code == 200:
+    soup = BeautifulSoup(resp.content, "html.parser")
+else:
+    print(resp)
+    soup = None
+
+resp = soup.find_all('div', class_='gsrt')
+# print(resp)
+# [<div aria-level="3" class="gsrt vk_bk dDoNo" role="heading">17:44</div>]
+for i in resp:
+    print(f"Hora en {ciudad}: ", i.text)
+results = []
+for g in soup.find_all('div', class_='r'):
+    anchors = g.find_all('a')
+    if anchors:
+        link = anchors[0]['href']
+        title = g.find('h3').text
+        item = {
+            "title": title,
+            "link": link
+        }
+        results.append(item)
+print(results)  # los resultados son titulo y enlace
